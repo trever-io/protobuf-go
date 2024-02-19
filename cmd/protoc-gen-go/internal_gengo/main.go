10779da50818ded0f6f8fde20d5a4349af107e30
Mathias Kahr <m.kahr@trever.io>
2024-02-19 10:36:12 +0100

    Add tags support

--- cmd/protoc-gen-go/internal_gengo/main.go
+++ cmd/protoc-gen-go/internal_gengo/main.go
@@ -418,6 +418,9 @@ func genMessageField(g *protogen.GeneratedFile, f *fileInfo, m *messageInfo, fie
 		tags = append(tags, gotrackTags...)
 	}
 
+	var leftTailing protogen.Comments
+	tags, leftTailing = AppendGoTagsFromTailingComment(tags, field.Comments.Trailing)
+
 	name := field.GoName
 	if field.Desc.IsWeak() {
 		name = genid.WeakFieldPrefix_goname + name
@@ -428,7 +431,8 @@ func genMessageField(g *protogen.GeneratedFile, f *fileInfo, m *messageInfo, fie
 		field.Desc.Options().(*descriptorpb.FieldOptions).GetDeprecated())
 	g.P(leadingComments,
 		name, " ", goType, tags,
-		trailingComment(field.Comments.Trailing))
+		//trailingComment(field.Comments.Trailing))
+		trailingComment(leftTailing))
 	sf.append(field.GoName)
 }
 
